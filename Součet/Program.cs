10d942ccfa8766b093e42762c97cc9af640267c5
Jiří Baran <jurka.baran@seznam.cz>
2020-03-30 17:41:57 +0200

    Přidejte soubory projektu.

---
+++ Součet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Součet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+
+            Console.Write("Zadejte 1. číslo: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Zadejte 2. číslo: ");
+            b = int.Parse(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine("Výsledek je: {0}", c);
+            Console.ReadKey();
+        }
+    }
+}
